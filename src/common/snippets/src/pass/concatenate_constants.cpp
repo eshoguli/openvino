e4f2a1153cc478b96b9dc270c46a6dd05d658cc4
Edward Shogulin <edward.shogulin@intel.com>
2022-04-06 00:50:57 +0300

    wip

--- src/common/snippets/src/pass/concatenate_constants.cpp
+++ src/common/snippets/src/pass/concatenate_constants.cpp
@@ -114,15 +114,21 @@ ConcatenateConstants::ConcatenateConstants() {
         std::unordered_map<std::string, size_t> indexByFriendlyName;
         std::string concatenatedConstantFriendlyName = "";
         bool concatenatedConstantWasAdded = false;
+        ngraph::element::Type inputPrecision;
 
         for (auto i = 0;  i < subgraph->get_input_size(); ++i) {
             auto target_input = subgraph->input(i);
             auto source_output = target_input.get_source_output();
             auto constant = as_type_ptr<opset1::Constant>(source_output.get_node()->shared_from_this());
 
-            // TODO: first version: 1) add shape limitation check 2) per 1 axis only
+            // TODO: first version: 1) add shape limitation check 2) per 1 axis only 2) no precision check
 
             if (constant != nullptr) {
+                if (!concatenatedConstantWasAdded) {
+                    concatenatedConstantFriendlyName = constant->get_friendly_name();
+                    concatenatedConstantWasAdded = true;
+                    inputPrecision = constant->output(0).get_element_type();
+                }
                 const auto& shape = constant->output(0).get_shape();
                 const size_t split_length = shape.size() >= (axis + 1ul) ? shape[axis] : 1ul;
                 split_lengths.push_back(shape[axis]);
@@ -138,31 +144,37 @@ ConcatenateConstants::ConcatenateConstants() {
             }
         }
 
-        auto newSubgraph = subgraph->clone_with_new_inputs(previousInputs);
-        replace_node(subgraph, newSubgraph);
-        copy_runtime_info(subgraph, newSubgraph);
-
         OutputVector foldingOutputs(1);
         auto concat = std::make_shared<ngraph::opset1::Concat>(outputs, axis);
         if (!concat->constant_fold(foldingOutputs, outputs)) {
-            // TODO: THROW_EXCEPTION << ""; //something wrong
             throw ngraph_error("constants were not concatenated");
         }
         const auto result = ov::as_type_ptr<opset1::Constant>(foldingOutputs[0].get_node_shared_ptr());
         if (result == nullptr) {
-            // TODO: THROW_EXCEPTION << "result of constant folding is not constant";
+            throw ngraph_error("constants were not concatenated");
         }
+
+        result->set_friendly_name(concatenatedConstantFriendlyName);
+        previousInputs.push_back(result);
+
+
+
+        auto parameter = std::make_shared<opset1::Parameter>(inputPrecision, result->output(0).get_partial_shape());
+        parameter->set_friendly_name(concatenatedConstantFriendlyName);
 
         std::shared_ptr<Node> split =
             std::all_of(split_lengths.begin(), split_lengths.end(), [&split_lengths](const size_t value) { return split_lengths[0] == value; }) ?
                 std::dynamic_pointer_cast<Node>(std::make_shared<opset1::Split>(
-                    result,
+                    parameter,
                     std::make_shared<opset1::Constant>(element::i32, Shape{}, std::vector<size_t>{axis}),
                     split_lengths.size())) :
                 std::make_shared<opset1::VariadicSplit>(
-                    result,
+                    parameter,
                     std::make_shared<opset1::Constant>(element::i32, Shape{ 1ul }, std::vector<size_t>{axis}),
                     std::make_shared<opset1::Constant>(element::i32, Shape{ split_lengths.size() }, split_lengths));
+
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming3").run_on_model(body);
+        body->validate_nodes_and_infer_types();
 
         auto parameters = body->get_parameters();
         for (auto i = 0; i < parameters.size(); ++i) {
@@ -174,16 +186,32 @@ ConcatenateConstants::ConcatenateConstants() {
 
             auto indexIt = indexByFriendlyName.find(parameter->get_friendly_name());
             if (indexIt == indexByFriendlyName.end()) {
-                // TODO: throw exception
+                throw ngraph_error("parameter was not mapped");
             }
+
+            //auto tragetInputs1 = parameter->output(0).get_target_inputs();
 
             for (auto input : parameter->output(0).get_target_inputs()) {
                 input.replace_source_output(split->output(indexIt->second));
             }
 
             body->remove_parameter(parameter);
+            std::cout << "removed: " << parameter->get_friendly_name() << std::endl;
+
+            //auto tragetInputs2 = parameter->output(0).get_target_inputs();
+
+            //ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming3").run_on_model(body);
+            //body->validate_nodes_and_infer_types();
         }
+        body->add_parameters({ parameter });
+
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming3").run_on_model(body);
         body->validate_nodes_and_infer_types();
+
+        auto newSubgraph = std::make_shared<ngraph::snippets::op::Subgraph>(previousInputs, body);
+        //auto newSubgraph = subgraph->clone_with_new_inputs(previousInputs);
+        replace_node(subgraph, newSubgraph);
+        copy_runtime_info(subgraph, newSubgraph);
 
         ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming3").run_on_model(body);
 
