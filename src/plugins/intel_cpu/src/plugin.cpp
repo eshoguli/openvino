f900a152947548c3082770c46548eac6a87168c3
Edward Shogulin <edward.shogulin@intel.com>
2022-04-05 13:51:26 +0300

    debugging

--- src/plugins/intel_cpu/src/plugin.cpp
+++ src/plugins/intel_cpu/src/plugin.cpp
@@ -84,6 +84,7 @@
 #include <snippets/pass/collapse_subgraph.hpp>
 #include <snippets/pass/common_optimizations.hpp>
 #include <snippets/pass/constant_folding.hpp>
+#include <snippets/pass/concatenate_constants.hpp>
 #include "ngraph_transformations/snippets_mark_skipped.hpp"
 
 #include <ngraph/opsets/opset1.hpp>
@@ -179,6 +180,8 @@ Engine::~Engine() {
 
 static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function> nGraphFunc, const bool _enableLPT,
                                                const bool _enableSnippets, const bool isLegacyApi) {
+    ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.original").run_on_model(nGraphFunc);
+
     ngraph::pass::Manager manager;
     manager.set_per_pass_validation(false);
     manager.register_pass<ngraph::pass::InitNodeInfo>();
@@ -499,6 +502,8 @@ static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function>
         lptManager.run_passes(nGraphFunc);
     }
 
+    ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.common").run_on_model(nGraphFunc);
+
     ngraph::pass::Manager postLPTPassManager;
     postLPTPassManager.register_pass<ngraph::pass::UnrollTensorIterator>();
     postLPTPassManager.register_pass<ReshapePRelu>();
@@ -518,15 +523,17 @@ static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function>
     postLPTPassManager.run_passes(nGraphFunc);
 
     if (_enableSnippets && dnnl::impl::cpu::x64::mayiuse(dnnl::impl::cpu::x64::avx2)) {
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.snippets.original").run_on_model(nGraphFunc);
+
         ngraph::pass::Manager tokenization_manager;
         tokenization_manager.register_pass<SnippetsMarkSkipped>();
         tokenization_manager.register_pass<ngraph::snippets::pass::EnumerateNodes>();
         tokenization_manager.register_pass<ngraph::snippets::pass::TokenizeSnippets>();
         tokenization_manager.get_pass_config()->set_callback<ngraph::snippets::pass::TokenizeSnippets>(
                 [](const std::shared_ptr<const ov::Node>& n) -> bool {
-                    if (ngraph::is_type<ngraph::opset1::FakeQuantize>(n) && !ngraph::pass::FakeQuantizeDecomposition::isAllScalarConstant(n)) {
-                        return true;
-                    }
+                    //if (ngraph::is_type<ngraph::opset1::FakeQuantize>(n) && !ngraph::pass::FakeQuantizeDecomposition::isAllScalarConstant(n)) {
+                    //    return true;
+                    //}
 
                     const auto& inputs = n->inputs();
                     // todo: clarify whether we can evaluate snippets on const paths
@@ -549,6 +556,16 @@ static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function>
         tokenization_manager.register_pass<ngraph::snippets::pass::CommonOptimizations>();
         tokenization_manager.register_pass<ngraph::snippets::pass::ConstantFolding>();
         tokenization_manager.run_passes(nGraphFunc);
+
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.snippets.transformed").run_on_model(nGraphFunc);
+
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming1").run_on_model(nGraphFunc);
+        {
+            ngraph::pass::Manager tokenization_manager;
+            tokenization_manager.register_pass<ngraph::snippets::pass::ConcatenateConstants>();
+            tokenization_manager.run_passes(nGraphFunc);
+        }
+        ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transforming4").run_on_model(nGraphFunc);
     }
 
     ngraph::pass::Manager fqDecompositionManager;
@@ -559,6 +576,8 @@ static void TransformationUpToCPUSpecificOpSet(std::shared_ptr<ngraph::Function>
         });
     fqDecompositionManager.register_pass<ngraph::pass::ConstantFolding>();
     fqDecompositionManager.run_passes(nGraphFunc);
+
+    ngraph::pass::VisualizeTree("c:\\Projects\\temp\\cpu.transformed").run_on_model(nGraphFunc);
 }
 
 static void Transformation(CNNNetwork& clonedNetwork, const bool _enableLPT, const bool _enableSnippets, const bool isLegacyApi) {
