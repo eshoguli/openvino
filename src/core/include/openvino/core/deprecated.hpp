d05edbcc0d9a4ec2826c9f9ce8f09d57b6905ef5
eshoguli <edward.shogulin@intel.com>
2023-06-15 16:07:24 +0100

    [COMMON] Deprication was turned off

--- src/core/include/openvino/core/deprecated.hpp
+++ src/core/include/openvino/core/deprecated.hpp
@@ -4,37 +4,7 @@
 
 #pragma once
 
-//
-// The OPENVINO_DEPRECATED macro can be used to deprecate a function declaration. For example:
-//
-//     OPENVINO_DEPRECATED("replace with groxify");
-//     void frobnicate()
-//
-// The macro will expand to a deprecation attribute supported by the compiler,
-// so any use of `frobnicate` will produce a compiler warning.
-//
-
-#if defined(__GNUC__)
-#    define OPENVINO_DEPRECATED(msg) __attribute__((deprecated(msg)))
-#    if __GNUC__ >= 6 || defined(__clang__)
-#        define OPENVINO_ENUM_DEPRECATED(msg) OPENVINO_DEPRECATED(msg)
-#    else
-#        define OPENVINO_ENUM_DEPRECATED(msg)
-#    endif
-#elif defined(_MSC_VER)
-#    define OPENVINO_DEPRECATED(msg) __declspec(deprecated(msg))
-#    if _MSC_VER >= 1900 /* VS2015 */
-#        define OPENVINO_ENUM_DEPRECATED(msg) [[deprecated(msg)]]
-#    else
-#        define OPENVINO_ENUM_DEPRECATED(msg)
-#    endif
-#elif defined(__INTEL_COMPILER)
-#    define OPENVINO_DEPRECATED(msg)      __attribute__((deprecated(msg)))
-#    define OPENVINO_ENUM_DEPRECATED(msg) OPENVINO_DEPRECATED(msg)
-#else
-#    define OPENVINO_DEPRECATED(msg)
-#    define OPENVINO_ENUM_DEPRECATED(msg)
-#endif
+#define OPENVINO_DEPRECATED(msg)
 
 // Suppress warning "-Wdeprecated-declarations" / C4996
 #if defined(__GNUC__)
