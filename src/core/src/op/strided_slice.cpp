ad82d18c518f71d9c3b5a5460928bd186d7691f1
eshoguli <edward.shogulin@intel.com>
2023-06-24 01:03:31 +0100

    checkout

--- src/core/src/op/strided_slice.cpp
+++ src/core/src/op/strided_slice.cpp
@@ -261,7 +261,3 @@ bool op::v1::StridedSlice::evaluate_label(TensorLabelVector& output_labels) cons
     return default_label_evaluator(this, output_labels);
     OPENVINO_SUPPRESS_DEPRECATED_END
 }
-
-bool op::v1::StridedSlice::constant_fold(OutputVector& output_values, const OutputVector& inputs_values) {
-    return false;
-}
