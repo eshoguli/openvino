fee59838a2857b2f0785b54ff806d2ab025a6830
eshoguli <edward.shogulin@intel.com>
2024-02-06 19:22:50 +0100

    [CPU] [ARM64] ACL eltwise issue

--- src/plugins/intel_cpu/src/nodes/eltwise.cpp
+++ src/plugins/intel_cpu/src/nodes/eltwise.cpp
@@ -2722,6 +2722,80 @@ void Eltwise::selectOptimalPrimitiveDescriptor() {
     selectPreferPrimitiveDescriptor(getImplPriority(), true);
 }
 
+
+namespace {
+std::ostream& operator<<(std::ostream& os, const Algorithm& algorithm) {
+    switch (algorithm) {
+        case Algorithm::EltwiseMulAdd:
+            os << "EltwiseMulAdd";
+            break;
+        case Algorithm::EltwiseAdd:
+            os << "EltwiseAdd";
+            break;
+        case Algorithm::EltwiseMultiply:
+            os << "EltwiseMultiply";
+            break;
+        case Algorithm::EltwisePowerStatic:
+            os << "EltwisePowerStatic";
+            break;
+        case Algorithm::EltwiseSigmoid:
+            os << "EltwiseSigmoid";
+            break;
+        case Algorithm::EltwiseExp:
+            os << "EltwiseExp";
+            break;
+        case Algorithm::EltwiseSwish:
+            os << "EltwiseSwish";
+            break;
+        case Algorithm::EltwiseHswish:
+            os << "EltwiseHswish";
+            break;
+        case Algorithm::EltwiseEqual:
+            os << "EltwiseEqual";
+            break;
+        default:
+            os << "[other]";
+            break;
+    }
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const dnnl::memory::data_type& type) {
+    switch (type) {
+        case dnnl::memory::data_type::f32:
+            os << "f32";
+            break;
+        case dnnl::memory::data_type::f16:
+            os << "f16";
+            break;
+        case dnnl::memory::data_type::s8:
+            os << "s8";
+            break;
+        case dnnl::memory::data_type::u8:
+            os << "u8";
+            break;
+        default:
+            os << "[other]";
+            break;
+    }
+    return os;
+}
+
+size_t data_type_size(const dnnl::memory::data_type& type) {
+    switch (type) {
+        case dnnl::memory::data_type::f32:
+            return 4;
+        case dnnl::memory::data_type::f16:
+            return 2;
+        case dnnl::memory::data_type::s8:
+        case dnnl::memory::data_type::u8:
+            return 1;
+        default:
+            OPENVINO_ASSERT(true, "unknow type");
+    }
+}
+} // namespace
+
 void Eltwise::execute(dnnl::stream strm) {
     if (execPtr) {
         jit_eltwise_call_args_ptrs args_ptrs = {};
@@ -2740,6 +2814,18 @@ void Eltwise::execute(dnnl::stream strm) {
             }
             args_ptrs.dst_offsets = execParams.outOffsets.data();
         }
+
+        // TODO: debug
+        if (std::dynamic_pointer_cast<EltwiseJitExecutor>(execPtr) != nullptr) {
+            std::cout << "JIT is used: " << this->getTypeStr() << ":" << this->getName() << ", " << this->getAlgorithm() << std::endl;
+        } else if (
+                (std::dynamic_pointer_cast<EltwiseRefExecutor<dnnl::impl::float16_t>>(execPtr) != nullptr) ||
+                (std::dynamic_pointer_cast<EltwiseRefExecutor<float>>(execPtr) != nullptr)) {
+            std::cout << "REFERENCE is used: " << this->getTypeStr() << ":" << this->getName() << ", " << this->getAlgorithm() << std::endl;
+        } else {
+            std::cout << "UNKNOWN is used: " << this->getTypeStr() << ":" << this->getName() << ", " << this->getAlgorithm() << std::endl;
+        }
+
         execPtr->exec(args_ptrs, dims_out);
     } else if (aclExecPtr) {
         std::vector<MemoryCPtr> srcMemory;
@@ -2749,6 +2835,7 @@ void Eltwise::execute(dnnl::stream strm) {
         std::vector<MemoryPtr> dstMemory;
         dstMemory.push_back(getDstMemoryAtPort(0));
 
+        std::cout << "ACL is used: " << this->getTypeStr() << ":" << this->getName() << ", " << this->getAlgorithm() << std::endl;
         aclExecPtr->exec(srcMemory, dstMemory, fqDataPtrs.data());
     } else {
         OPENVINO_THROW("Can't execute eltwise node with name: ", getName(), ". Primitive isn't created");
