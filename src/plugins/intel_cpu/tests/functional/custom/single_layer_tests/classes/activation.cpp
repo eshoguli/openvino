f9df77a42a6a3f6bb57660dfbe0d1ac671bfc5cc
eshoguli <edward.shogulin@intel.com>
2024-02-14 10:25:30 +0100

    [CPU] [ARM64] jit emitters: exp

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
@@ -52,10 +52,15 @@ void ActivationLayerCPUTest::generate_inputs(const std::vector<ov::Shape>& targe
     uint32_t range = 0;
     int32_t resolution = 0;
 
-    if (activationType == utils::ActivationTypes::Exp && netPrecision == ov::element::bf16) {
-        startFrom = 0;
-        range = 2;
-        resolution = 32768;
+    if (activationType == utils::ActivationTypes::Exp) {
+        if (netPrecision == ov::element::bf16) {
+            startFrom = 0;
+            range = 2;
+        } else {
+            startFrom = -10;
+            range = 25;
+            resolution = 32768;
+        }
     } else if (activationType == utils::ActivationTypes::Acosh) {
         startFrom = 2;
         range = 2;
@@ -141,7 +146,8 @@ std::string ActivationLayerCPUTest::getPrimitiveType(const utils::ActivationType
 #if defined(OPENVINO_ARCH_ARM64)
     if ((element_type == ov::element::f32) &&
         ((activation_type == utils::ActivationTypes::Clamp) ||
-        (activation_type == utils::ActivationTypes::Relu))) {
+        (activation_type == utils::ActivationTypes::Relu) ||
+        (activation_type == utils::ActivationTypes::Exp))) {
         return "jit";
     }
 
