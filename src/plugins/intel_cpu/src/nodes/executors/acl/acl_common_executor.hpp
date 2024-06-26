141d7cdd469cf3ba8de1d9060c99d5602be0c611
Edward Shogulin <edward.shogulin@intel.com>
2024-06-26 01:21:15 +0100

    [CPU] [ARM] FullyConnected: int8 support

--- src/plugins/intel_cpu/src/nodes/executors/acl/acl_common_executor.hpp
+++ src/plugins/intel_cpu/src/nodes/executors/acl/acl_common_executor.hpp
@@ -49,6 +49,11 @@ public:
 
 protected:
     ACLTensorAttrs aclTensorAttrs;
+
+    virtual ACLInfo initTensorInfo(const arm_compute::TensorShape& tensorShape,
+                                   const arm_compute::DataType& dataType,
+                                   const arm_compute::DataLayout& dataLayout);
+
 private:
     ACLMemoryTensors aclMemoryTensors;
     ACLFunction iFunction = nullptr;
